c227424358efc758fec668c4c42f2b8676dbfb35
Moyuri Akther <akthermoyuri@gmail.com>
2024-07-12 00:56:44 +0600

    solve 5 problem

---
+++ Task[1]Array-Filtering-And-Mapping.js
@@ -0,0 +1,62 @@
+// 1.Task: Array Filtering And Mapping
+const persons = [
+    { name: 'Alice', age: 25, gender: 'female' },
+    { name: 'Bob', age: 30, gender: 'male' },
+    { name: 'Charlie', age: 28, gender: 'male' },
+    { name: 'Diana', age: 22, gender: 'female' },
+    { name: 'Eve', age: 35, gender: 'female' },
+    { name: 'Frank', age: 40, gender: 'male' }
+  ];
+
+  const males = persons.filter((person) => person.gender !== "female").map(p =>p.name)
+  console.log(males)
+
+  // 2.Task: Object Manipulation
+const books = [
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+  { title: '1984', author: 'George Orwell', year: 1949 },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  { title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 },
+  { title: 'Moby-Dick', author: 'Herman Melville', year: 1851 }
+];
+
+const  bookTitles = (books) =>{
+  const titles = books.map(book => book.title)
+  return titles;
+}
+const result = bookTitles(books);
+console.log(result)
+
+// 4.Task: Sorting Objects
+const cars = [
+  { make: 'Toyota', model: 'Camry', year: 2019 },
+  { make: 'Honda', model: 'Accord', year: 2017 },
+  { make: 'Ford', model: 'Mustang', year: 2021 },
+  { make: 'Chevrolet', model: 'Malibu', year: 2018 },
+  { make: 'Nissan', model: 'Altima', year: 2020 }
+];
+
+const sortByYear = (cars) =>{
+  const result = cars.sort((a, b) => a.year - b.year);
+  return result;
+}
+const result4 = sortByYear(cars)
+console.log(result4)
+
+// 5.Task: Find And Modify
+const findByName = (persons , updateName, updateAge) =>{
+  const result = persons.find(person => person.name === updateName)
+  if(result){
+    result.age = updateAge;
+  }
+}
+findByName(persons, 'Alice', 27);
+console.log(persons);
+
+// 6.Task: Array Reduction
+const numbers = [12, 1, 3, 10, 14, 13, 18, 20, 22, 21, 26, 24];
+const sumOfEvenNumbers = (numbers) =>{
+  return numbers.reduce((acc, curr) =>  curr % 2 === 0 ? acc + curr : acc, 0);
+}
+const result6 = sumOfEvenNumbers(numbers);
+console.log(result6)
